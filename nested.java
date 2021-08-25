b500a5ffcba92a68d889e8668ac27a4d201d7cab
gloech0 <48336197+wrodesxs@users.noreply.github.com>
2021-08-25 21:31:44 +0100

    Add files via upload

---
+++ nested.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class nested {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int rows;
+        int columns;
+        String symbol = "";
+
+        System.out.println("Number of rows: ");
+        rows = scanner.nextInt();
+        System.out.println("Number of columns: ");
+        columns = scanner.nextInt();
+        System.out.println("Symbol: ");
+        symbol = scanner.next();
+
+        for(int i = 1; i <= rows; i++){
+            System.out.println();
+            for(int j = 1; j<= columns; j++){
+                System.out.print(symbol);
+            }
+        }
+
+        scanner.close();
+    }
+}
